cc4d286144a2cd76580a5617b69f245d991b5df7
Valentina Cupac <valentina.cupac@optivem.com>
2019-01-03 10:27:43 +0100

    #43 SuppliersController - Use AspNetCoreCrudController

--- src/Optivem.Northwind/Controllers/SuppliersController.cs
+++ src/Optivem.Northwind/Controllers/SuppliersController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Optivem.Commons.RestService.AspNetCore;
 using Optivem.Northwind.Core.Application.Dto;
 using Optivem.Northwind.Core.Application.Service;
 using Optivem.Northwind.Core.Domain.Repository;
@@ -11,91 +13,11 @@ namespace Optivem.Northwind.Controllers
 {
     [Route("api/suppliers")]
     [ApiController]
-    public class SuppliersController : ControllerBase
+    public class SuppliersController : AspNetCoreCrudController<INorthwindUnitOfWork, ISupplierService, SupplierRequest, SupplierResponse, int>
     {
-        private readonly INorthwindUnitOfWork unitOfWork;
-        private readonly ISupplierService service;
-
-        public SuppliersController(INorthwindUnitOfWork unitOfWork, ISupplierService service)
+        public SuppliersController(INorthwindUnitOfWork unitOfWork, ISupplierService service) 
+            : base(unitOfWork, service, e => e.SupplierId, e => e.SupplierId)
         {
-            this.unitOfWork = unitOfWork;
-            this.service = service;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<SupplierResponse>>> GetSuppliers()
-        {
-            var result = await service.GetAsync();
-            return result.ToList();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<SupplierResponse>> GetSupplier(int id)
-        {
-            var supplier = await service.GetAsync(id);
-
-            if (supplier == null)
-            {
-                return NotFound();
-            }
-
-            return supplier;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSupplier(int id, SupplierRequest supplier)
-        {
-            if (id != supplier.SupplierId)
-            {
-                return BadRequest();
-            }
-            
-            try
-            {
-                service.Update(supplier);
-
-                await unitOfWork.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!service.Exists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<SupplierRequest>> PostSupplier(SupplierRequest supplier)
-        {
-            var result = service.Add(supplier);
-
-            await unitOfWork.SaveChangesAsync();
-
-            return CreatedAtAction("GetSupplier", new { id = supplier.SupplierId }, result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<SupplierRequest>> DeleteSupplier(int id)
-        {
-            var exists = service.Exists(id);
-            
-            if (!exists)
-            {
-                return NotFound();
-            }
-            
-            service.Delete(id);
-
-            await unitOfWork.SaveChangesAsync();
-
-            return NoContent();
         }
     }
 }
