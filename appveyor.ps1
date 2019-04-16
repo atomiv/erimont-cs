088f77fed7f873133f43fcc2d2055795fc71778a
Valentina Cupać <valentina.cupac@optivem.com>
2019-04-16 19:55:24 +0200

    #53 AppVeyor - SQL Server

---
+++ appveyor.ps1
@@ -0,0 +1,8 @@
+﻿$filePath = 'test\\Web\\AspNetCore\\Rest.Test\\appsettings.Test.json'
+$connection = 'Server=(local)\SQL2017;Database=Northwind.Test;User ID=sa;Password=Password12!'
+
+$json = (Get-Content -Path $filePath) | ConvertFrom-Json
+
+$json.ConnectionStrings.Northwind = $connection
+
+$json | ConvertTo-Json | Set-Content $filePath
