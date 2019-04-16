83ddb57657c7a0d8ed6377ca50582130fee48a60
Valentina Cupać <valentina.cupac@optivem.com>
2019-04-16 20:29:11 +0200

    #53 AppVeyor - SQL Server

--- configure.ps1
+++ configure.ps1
@@ -1,8 +1,28 @@
-﻿$filePath = 'test\\Web\\AspNetCore\\Rest.Test\\appsettings.Test.json'
+﻿Write-Host "Starting updates..."
+
+$filePath = 'test\\Web\\AspNetCore\\Rest.Test\\appsettings.Test.json'
+
+Write-Host "File path is: $filePath"
+
+$filePathExists = Test-Path -Path $filePath
+
+if(!$filePathExists)
+{
+	Write-Error -Message "File path does not exist: $projectPath"  -ErrorAction Stop
+}
+
 $connection = 'Server=(local)\SQL2017;Database=Northwind.Test;User ID=sa;Password=Password12!;MultipleActiveResultSets=True;'
+
+Write-Host "Connection is: $connection"
 
 $json = (Get-Content -Path $filePath) | ConvertFrom-Json
 
+Write-Host "JSON is: $json"
+
 $json.ConnectionStrings.Northwind = $connection
 
+Write-Host "Updated JSON is $json"
+
 $json | ConvertTo-Json | Set-Content $filePath
+
+Write-Host "File has been updated"
